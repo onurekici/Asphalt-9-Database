1a1b8bf525f8d1be2e4b8947bccce1aad35c6316
muhendisbirbey <42184009+muhendisbirbey@users.noreply.github.com>
2018-10-17 03:21:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client ();
 
 
 bot.on('ready', () => {
-  bot.user.setGame('Çalışmalar devam ediyor. Twitch kanalımıza gitmek için İZLE butonuna basabilirsin :) :', 'https://www.youtube.com/channel/UCTyQuKVP6ePA2jgb9TE3SHg')
+  bot.user.setGame('Çalışmalar devam ediyor. Twitch kanalımıza gitmek için İZLE butonuna basabilirsin :slight_smile: ', 'https://www.twitch.tv/muhendisbeymuhendishanim')
   bot.user.setStatus('idle')
 })
 
