3be4c3927cf9dd86134fecccb29744f56440286a
muhendisbirbey <42184009+muhendisbirbey@users.noreply.github.com>
2018-10-17 13:27:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,9 @@ client.on('ready', () => {
   client.user.setGame('Çalışmalar devam ediyor. Twitch kanalımıza gitmek için İZLE butonuna basabilirsin :) ', 'https://www.twitch.tv/muhendisbeymuhendishanim')
 });
 
-client.on('message', message => {
-  // If the message is "ping"
-  if (message.content === 'sa') {
-    // Send "pong" to the same channel
-    msg.reply('Aleyküm Selam hoşgeldin :slight_smile:  ');
+client.on('message', msg => {
+  if (msg.content === 'sa') {
+    msg.reply('Aleyküm Selam hoşgeldin');
   }
 });
 
