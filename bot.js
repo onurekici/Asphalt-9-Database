30f441571d09ac9b6dd3a68ddd22bcbb8b81c8f8
muhendisbirbey <42184009+muhendisbirbey@users.noreply.github.com>
2018-10-24 23:59:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ client.on('message', message => {
 
 client.on('guildMemberAdd', member => {
   // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.find(ch => ch.name === 'kırmızı-halı');
+  const channel = member.guild.channels.find(ch => ch.name === 'hoş-geldiniz-🎀');
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
