a254db01a5f79542fc8612216c4e4dacb4e9fafb
muhendisbirbey <42184009+muhendisbirbey@users.noreply.github.com>
2018-10-25 07:52:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-
-
-
-
 client.on('ready', () => {
   client.user.setGame('Çalışmalar devam ediyor. Twitch kanalımıza gitmek için İZLE butonuna basabilirsin :) ', 'https://www.twitch.tv/muhendisbeymuhendishanim')
 });
@@ -31,9 +27,6 @@ client.on('message', message => {
 
 client.on('message', message => {  
   guildMember = message.author;
-  //joinDiscord = moment(guildMember.user.createdAt).format('lll') + '\n*' + moment(new Date()).diff(guildMember.user.createdAt, 'days') + ' days ago*';
-
-    // Send the user's avatar URL
    message.reply(` Mesajı yazan kullanıcı :` + guildMember);
   }
 });
